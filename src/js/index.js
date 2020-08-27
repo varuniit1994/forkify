35242b53f23ed01c58b248f63011b90cd1c19581
varuniit1994@gmail.com <varuniit1994@gmail.com>
2020-08-27 17:20:01 +0530

    added first code to generate dynamic html from javascript.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,40 @@
 import Search from "./models/Search";
+import * as searchView from "./views/searchView"
+import { elements } from "./views/base";
+/*Global state of the app
+--Search object
+--Current recipe object
+--Shooping list object
+--Liked recipes
+*/
+const state={};
+
+const controlSearch=async ()=>{
+    //1) Get the query from the view
+    const query=searchView.getInput();  //TODO
+    console.log(query);
+    if(query)
+    {
+        //2) Create a New search object and add it to the state object
+        state.search =new Search(query);
+
+        //3)prepare UI for the result
+
+        //4)search for recipes
+        await state.search.getResults();
+
+        //5)Render results on UI
+        //console.log(state.search.result);
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener("submit",e=>{
+    e.preventDefault();
+    controlSearch();
+});
 
 
-const search =new Search("pizza");
-console.log(search);
-search.getResults();
+
 
 
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -13,7 +13,7 @@ export default class Search
         {
             const res=await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result=res.data.recipes;
-            console.log(this.result);
+           // console.log(this.result);
         }
         catch(error)
         {
