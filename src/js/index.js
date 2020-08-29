cd29d9deadde0b2216246ec88c32dff85e93772a
varuniit1994@gmail.com <varuniit1994@gmail.com>
2020-08-29 20:22:29 +0530

    added clear input functionality and loader functionality.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView"
-import { elements } from "./views/base";
+import { elements,renderLoader,clearLoader } from "./views/base";
 /*Global state of the app
 --Search object
 --Current recipe object
@@ -19,12 +19,15 @@ const controlSearch=async ()=>{
         state.search =new Search(query);
 
         //3)prepare UI for the result
-
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
         //4)search for recipes
         await state.search.getResults();
 
         //5)Render results on UI
         //console.log(state.search.result);
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,6 +2,34 @@ import { elements } from "./base";
 
 export const getInput=()=>elements.searchInput.value;
 
+export const clearInput=()=>{
+    elements.searchInput.value="";
+}
+
+export const clearResults=()=>{
+    elements.searchResultList.innerHTML="";
+}
+
+// pasta with tamato and spanish
+const limitRecipeTitle=(title,limit=17)=>{
+    const newLetter=[];
+    if(title.length>limit)
+    {
+        title.split(" ").reduce((acc,current)=>{
+            if(acc+current.length<=limit)
+            {
+                newLetter.push(current);
+            }
+            return acc+current.length;
+        },0);
+       
+        //return the result
+        return `${newLetter.join(" ")}...`;
+    }
+    return title;
+}
+
+
 const renderRecipe=(recipe)=>{
     const markup=`
     <li>
@@ -10,17 +38,24 @@ const renderRecipe=(recipe)=>{
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>           
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
     </li>`;
     elements.searchResultList.insertAdjacentHTML("beforeend",markup);
-}
+};
 
-export const renderResults=recipes=>{
-    console.log(recipes);
+export const renderResults=(recipes,page=1,resPerPage=10)=>
+{
+    //console.log(recipes);
+    const start=0;
+    const end=10;
+
+    
+
     recipes.forEach((recipe)=>{
         renderRecipe(recipe);
     });
-}
+
+};
